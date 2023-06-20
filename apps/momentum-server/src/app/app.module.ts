7ae69a1d00fab80f3815aaa868c7719e2c20f590
zakaev <zakaev@follettlearning.com>
2023-06-21 01:10:01 +0600

    added config module

--- apps/momentum-server/src/app/app.module.ts
+++ apps/momentum-server/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppService } from './app.service';
 import { AuthModule } from '../auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL),
     AuthModule,
   ],
