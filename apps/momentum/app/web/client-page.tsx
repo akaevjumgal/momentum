4e774d05cd0929955401c4d84b1ac68bad8f07a4
zakaev <zakaev@follettlearning.com>
2023-10-03 00:56:32 +0600

    removed local env file, moved header & toolbar to web, added vercel config ile

--- apps/momentum/app/web/client-page.tsx
+++ apps/momentum/app/web/client-page.tsx
@@ -1,11 +1,14 @@
 import { TasksView } from '@/components/tasks-view/tasks-view';
 import { WeekView } from '@/components/week-view/week-view';
-
+import { Header } from '@/components/header/header';
+import { Toolbar } from '@/components/toolbar/toolbar';
 export default function ClientPage() {
   return (
     <div className="bg-light-border dark:bg-dark-default">
+      <Header />
       <WeekView />
       <TasksView />
+      <Toolbar />
     </div>
   );
 }
